a191626247feb3cb56424f2503c5d73f247c42d3
udit-329 <33633390+udit-329@users.noreply.github.com>
2020-02-01 22:32:33 -0500

    Update sorry, eh.py

--- sorry, eh.py
+++ sorry, eh.py
@@ -9,10 +9,11 @@ data_An = JSON_data.data_import(link2)
 data_test = JSON_data.data_import(link1)
 
 #Get the following data from webpage
-cat = "SO"
-sub_cat = "gf/w"
-name = "name"
-reason = "reason"
+#cat, sub_cat, name, reason = get_web.get_data()
+cat = "professional"
+sub_cat = "boss"
+name = "Mr. T"
+reason = "not showing up to the meeting"
 #get it
 
 tupleA = data_analytics.recommending_engine(cat, sub_cat)
@@ -25,16 +26,20 @@ first2 = tupleA[0][1] #line two
 second2 = tupleA[1][1]
 third2 = tupleA[2][1]
 
-firstPer = tupleA[0][2] #percentage probability
-secondPer = tupleA[1][2]
-thirdPer = tupleA[2][2]
+firstPer = tupleA[0][2] * 100 #percentage probability
+secondPer = tupleA[1][2] * 100
+thirdPer = tupleA[2][2] * 100
 
 best_apology = compiler.create_apology(cat, sub_cat, first1, first2, name, reason)
 second_best_apology = compiler.create_apology(cat, sub_cat, second1, second2, name, reason)
 third_best_apology = compiler.create_apology(cat, sub_cat, third1, third2, name, reason)
 
 print(best_apology)
-
-
-
+print("\nThis worked", firstPer * 100, "% of the time.")
+print()
+print(second_best_apology)
+print("\nThis worked", secondPer * 100, "% of the time.")
+print()
+print(third_best_apology)
+print("\nThis worked", thirdPer * 100, "% of the time.")
 
